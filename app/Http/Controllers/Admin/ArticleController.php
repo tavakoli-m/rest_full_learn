fb9b3c83e3a783a316d3017142b42a56e9d545ca
unknown <heyreza8@gmail.com>
2025-04-04 00:11:04 +0330

    2.16

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -12,7 +12,7 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        $articles = Article::with(['user:id,email'])->get();
+        $articles = Article::with(['user'])->get();
 
         return response()->json([
             'data' => $articles
--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,8 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
+    protected $appends = ['full_name'];
+
     /**
      * The attributes that are mass assignable.
      *
@@ -45,5 +47,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function getFullNameAttribute(){
+        return $this->first_name . ' ' . $this->last_name;
     }
 }
