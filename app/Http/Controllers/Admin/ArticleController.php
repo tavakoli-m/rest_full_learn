51ada4b069906144f3b9798b090e2edd33d7d98b
unknown <heyreza8@gmail.com>
2025-04-04 00:03:04 +0330

    2.15

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -12,7 +12,7 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        $articles = Article::all();
+        $articles = Article::with(['user:id,email'])->get();
 
         return response()->json([
             'data' => $articles
