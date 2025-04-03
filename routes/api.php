341cb115be3d60ef18049d89b38c0837e84c0ec2
unknown <heyreza8@gmail.com>
2025-04-03 16:54:25 +0330

    2.3

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
+//
+
+Route::get('/test',function (){
+    return response()->json([
+       "id" => 1,
+       "message" => "okk",
+       "numbers" => [1,2,3,4,5]
+    ]);
+});
