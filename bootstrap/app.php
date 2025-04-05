42ea0afaa28387821deab81f4744906632b15994
unknown <heyreza8@gmail.com>
2025-04-06 00:15:42 +0330

    feat: set exception handling for route model binding not found

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Services\ApiResponse\Facades\ApiResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $exception) {
+           return ApiResponse::withMessage("not found !!")->withStatus(404)->send();
+        });
     })->create();
