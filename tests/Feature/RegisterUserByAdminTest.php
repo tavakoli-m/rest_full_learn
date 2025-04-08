3b7027a82041241eea317eef3cca27f7208f2474
unknown <heyreza8@gmail.com>
2025-04-08 21:11:50 +0330

    test.6

--- tests/Feature/RegisterUserByAdminTest.php
+++ tests/Feature/RegisterUserByAdminTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class RegisterUserByAdminTest extends TestCase
@@ -30,5 +31,23 @@ class RegisterUserByAdminTest extends TestCase
     {
         $user = User::whereEmail('admin@gmail.com')->first();
         $this->actingAs($user)->post('api/user',[])->assertStatus(422);
+    }
+
+    public function test_an_admin_can_register_new_user()
+    {
+        $user = User::whereEmail('admin@gmail.com')->first();
+
+        $response = $this->actingAs($user)->post('api/user',[
+            'first_name' => 'test',
+            'last_name' => 'test last name',
+            'email' => 'test@gmail.com',
+            'password' => Hash::make('12345678'),
+        ])->assertStatus(200)
+        ->assertJsonStructure(['message'])
+        ->assertJson(['message' => 'User created successfully']);
+
+        $registeredUser = User::findOrFail($response->json('data')['id']);
+
+        $this->assertEquals($registeredUser->email,'test@gmail.com');
     }
 }
